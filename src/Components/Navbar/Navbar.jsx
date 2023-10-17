21ceb3458dd4c777a7277acc178d5fff412c142e
mihir-x <ictlancerx@gmail.com>
2023-10-17 21:13:06 +0600

    added navbar to the root layout

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Link, NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+
+    const navLinks = <>
+        <li className="text-base font-semibold"><NavLink to='/' className={({isActive, isPending}) => isPending ? 'pending' : isActive ? 'font-bold underline text-lime-500 bg-lime-200' : ''}>Home</NavLink></li>
+        <li className="text-base font-semibold"><NavLink to='/addproduct' className={({isActive, isPending}) => isPending ? 'pending' : isActive ? 'font-bold underline text-lime-500 bg-lime-200' : ''}>Add Product</NavLink></li>
+        <li className="text-base font-semibold"><NavLink to='/mycart' className={({isActive, isPending}) => isPending ? 'pending' : isActive ? 'font-bold underline text-lime-500 bg-lime-200' : ''}>My Cart</NavLink></li>
+    </>
+
+    return (
+        <div>
+            <div className="navbar shadow-2xl bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {
+                                navLinks
+                            }
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {
+                            navLinks
+                        }
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link><button className="btn">Login</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             this is root
+            <Outlet></Outlet>
         </div>
     );
 };
