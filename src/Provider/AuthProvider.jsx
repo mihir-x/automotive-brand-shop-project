fd6ec269b7b3a17fef4a57eeee991e33c22d08bd
mihir-x <ictlancerx@gmail.com>
2023-10-17 21:48:22 +0600

    added authprovider

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,23 @@
+import { createContext } from "react";
+import PropTypes from 'prop-types';
 
+const AuthContext = createContext(null)
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
+
+    const authInfo = {
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
 export default AuthProvider;
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
