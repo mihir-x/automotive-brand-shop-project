2ae07dcc1d2f1f4f6a99633d2e61ff08122f990e
mihir-x <ictlancerx@gmail.com>
2023-10-20 02:53:31 +0600

    added driving passion section

---
+++ src/Components/DrivingPassion/DrivingPassion.jsx
@@ -0,0 +1,23 @@
+
+
+const DrivingPassion = () => {
+    return (
+        <div className="mt-5 md:mt-10 lg:mt-16 bg-white text-black">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                <div>
+                    <img src="https://i.ibb.co/CnyMBtR/prod9e6047.jpg" alt="" className="w-full h-full" />
+                </div>
+                <div className="text-center space-y-5">
+                    <h1 className='text-lg md:text-3xl lg:text-4xl font-bold mt-5'>Driving Passion</h1>
+                    <p>Driving passion is the exhilarating and deep seated enthusiasm individuals have for being behind the wheel. It transcends mere transportation, encapsulating the joy, freedom and connection to the road that driving can offer. Whether it&apos;s the thrill of speed , the serenity of a scenic drive or the love of finely tuned machines, driving passion is a powerful force that unites and inspires automotive enthusiasts worldwide.</p>
+                </div>
+                <div className="relative">
+                    <img src="https://i.ibb.co/cNXVkzz/Elwood-Haynes.jpg" alt="" className="w-full h-full" />
+                    <div className="absolute inset-0 bg-gradient-to-l from-transparent via-transparent to-white"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DrivingPassion;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "react-router-dom";
 import Banner from "../../Components/Banner/Banner";
 import Footer from "../../Components/Footer/Footer";
 import AllBrands from "../../Components/AllBrands/AllBrands";
+import DrivingPassion from "../../Components/DrivingPassion/DrivingPassion";
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <AllBrands allBrands= {allBrands}></AllBrands>
+            <DrivingPassion></DrivingPassion>
             <Footer></Footer>
         </div>
     );
